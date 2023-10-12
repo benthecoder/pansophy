0c096895817c5740e082139450775fa9cf4e88e4
benthecoder <benthecoder07@gmail.com>
2023-10-12 16:27:00 -0500

    directory issue on cloud

--- app.py
+++ app.py
@@ -17,16 +17,18 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
+# Absolute paths
+BASE_DIR = Path(__file__).resolve().parent
+GRAPHS_DIR = BASE_DIR / "graphs"
+
 
 def render_html(html_path: Path):
-    """Renders HTML file"""
     with open(html_path, "r") as file:
         html_content = file.read()
     components.html(html_content, height=800, width=1200)
 
 
 def get_file_paths(directory_path, topic):
-    """Get file paths for SVG and HTML based on topic"""
     topic_filename = topic.replace(" ", "_").lower()
     return (
         directory_path / f"{topic_filename}.svg.svg",
@@ -35,48 +37,36 @@ def get_file_paths(directory_path, topic):
 
 
 def delete_topic_graph(directory_path):
-    """Delete the topic graph directory and its contents"""
     import shutil
 
     shutil.rmtree(directory_path)
 
 
 def render_topic_graph(directory_path, topic):
-    """Render the topic graph from given directory and topic"""
     svg_path, html_path = get_file_paths(directory_path, topic)
 
-    st.image(str(svg_path))
+    if svg_path.exists():
+        st.image(str(svg_path))
+    else:
+        st.error(f"File {svg_path} not found!")
 
     st.subheader("Interactive Knowledge Graph")
     render_html(str(html_path))
 
 
 def main():
-    st.image("img/logo.jpeg", use_column_width=True)
-
-    st.markdown(
-        """
-    Welcome to Pansophy! 
-
-    Pansophy is an innovative tool designed to help you visualize and understand any topic in depth. 
-    By leveraging the power of AI, Pansophy constructs detailed [knowledge graphs](https://en.wikipedia.org/wiki/Knowledge_graph) for your chosen topic, 
-    presenting both core concepts and intricate details in an interconnected manner. 
-    
-    Whether you're a student, researcher, or just curious, Pansophy aims to provide a comprehensive perspective on any subject 
-    you're keen to explore.
-    """
-    )
+    st.title("Pansophy 📚🧠💡")
 
     tab1, tab2 = st.tabs(["Create", "History"])
 
-    existing_topics = [p.name for p in Path("graphs").iterdir() if p.is_dir()]
+    existing_topics = [p.name for p in GRAPHS_DIR.iterdir() if p.is_dir()]
 
     with tab1:
         st.header("Generate Knowledge Graph 🌐")
-        topic = st.text_input(label="Enter a topic", placeholder="love")
+        topic = st.text_input("enter a topic")
 
         if topic:
-            directory_path = Path("graphs") / topic.replace(" ", "_").lower()
+            directory_path = GRAPHS_DIR / topic.replace(" ", "_").lower()
 
             if directory_path.exists():
                 st.write(f"Fetching pregenerated knowledge graph for {topic}...")
@@ -103,12 +93,12 @@ def main():
 
     with tab2:
         st.header("Explore Knowledge Graphs")
-        selected_topic = st.selectbox("Select a topic", existing_topics, index=0)
+        selected_topic = st.selectbox("select a topic", existing_topics, index=0)
 
         submit = st.button("View")
 
         if submit:
-            topic_directory = Path("graphs") / selected_topic.replace(" ", "_").lower()
+            topic_directory = GRAPHS_DIR / selected_topic.replace(" ", "_").lower()
             render_topic_graph(topic_directory, selected_topic)
 
 
